e345d677661c19cf1d9c5e839a554250da432af3
JeanJeadanjeh <69907719+JeanJeadanjeh@users.noreply.github.com>
2021-03-20 20:04:06 +0100

    Updated input check

--- Directory/res/js/createAnAccountPages.js
+++ Directory/res/js/createAnAccountPages.js
@@ -14,6 +14,10 @@ birth = document.getElementById("birth");
 username = document.getElementById("username");
 email_input = document.getElementById("email");
 
+password1 = document.getElementById("password");
+password2 = document.getElementById("retype-password");
+submit = document.getElementById("submit-button")
+
 function validateEmail(email) {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email.toLowerCase());
@@ -23,52 +27,55 @@ function isValid(element){
     var regEx = /^[A-Za-z \s \-]+$/;
     if (element===fname)
     {
+        invalid_first_name = document.getElementById("invalid-first-name");
+        invalid_first_name.style.display = "none";
         if(fname.value.match(regEx))
         {
+            invalid_first_name.style.display = "none";
             return true;
         }
         else
         {
+            invalid_first_name.style.display = "initial";
             return false;
         }
     }
     if (element===lname)
     {
+        invalid_last_name = document.getElementById("invalid-last-name");
+        invalid_last_name.style.display = "none";  
         if(lname.value.match(regEx))
         {
+            invalid_last_name.style.display = "none";  
             return true;
         }
         else
         {
-            return false;
-        }
-    }
-
-    if (element===lname)
-    {
-        if(lname.value.match(regEx))
-        {
-            return true;
-        }
-        else
-        {
+            invalid_last_name.style.display = "initial";
             return false;
         }
     }
 
     if (element===username)
     {
+        invalid_username = document.getElementById("invalid-username");
+        invalid_username.style.display = "none"; 
         if(username.value.match(regEx))
         {
+            invalid_username.style.display = "none";  
             return true;
         }
         else
         {
+            invalid_username.style.display = "initial";  
             return false;
         }
     }
 
     if (element===email_input){
+        invalid_email = document.getElementById("invalid-email");
+        invalid_email.style.display = "none"; 
+        validateEmail(email_input.value) ? invalid_email.style.display = "none" :  invalid_email.style.display = "initial"; ; 
         return validateEmail(email_input.value);
     }
     return false;
@@ -85,20 +92,28 @@ function toggle(element){
 
 function checkValidity(element){
     if (element === next1){
-        if (isValid(fname) && isValid(lname))
+        var cond1 = isValid(fname);
+        var cond2 = isValid(lname);
+        if (cond1 && cond2)
         {
             toggle(page1);
             toggle(page2);
         }
     }
     if (element === next2){
-        if (isValid(email_input) && isValid(username))
+        var cond1 = isValid(email_input);
+        var cond2 = isValid(username);
+        if (cond1 && cond2)
         {
             toggle(page2);
             toggle(page3);
         }
     }
+    else{
+        console.log((password1.value===password2.value) && password1.value.length>8);
+    }
 }
 
 next1.addEventListener("click", ()=>{checkValidity(next1)})
 next2.addEventListener("click", ()=>{checkValidity(next2)})
+submit.addEventListener("click", ()=>{checkValidity(submit)})
